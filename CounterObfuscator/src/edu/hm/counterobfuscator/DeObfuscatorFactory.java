8b98930694f2d5f599bc71297a244119be3b19ff
Benjamin Möllerke <bemoelle@gmail.com>
2015-04-07 21:47:23 +0200

    updated DeobfuscatorFactory

--- CounterObfuscator/src/edu/hm/counterobfuscator/DeObfuscatorFactory.java
+++ CounterObfuscator/src/edu/hm/counterobfuscator/DeObfuscatorFactory.java
@@ -2,12 +2,6 @@ package edu.hm.counterobfuscator;
 
 public class DeObfuscatorFactory {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
 
 }
